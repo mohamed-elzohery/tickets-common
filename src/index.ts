48b0a918638c57817644a34411b7858724356a7c
mohamed-elzohery <m.zohery1998@gmail.com>
2022-05-15 11:25:43 +0200

    Adding advRes middleware

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@
 export * from './middlewares/app-error-handler';
 export * from './middlewares/async-error-handler';
 export * from './middlewares/authGuard';
+export * from './middlewares/advResults';
 
 // Utils
 export * from './utils/ErrorResponse';
