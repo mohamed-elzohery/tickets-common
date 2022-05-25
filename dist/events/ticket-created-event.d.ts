2fcf29bf68cbe69b2efc8c50a7b603f980249e5a
mohamed-elzohery <m.zohery1998@gmail.com>
2022-05-25 14:31:14 +0200

    Fixing event interfaces

--- dist/events/ticket-created-event.d.ts
+++ dist/events/ticket-created-event.d.ts
@@ -5,5 +5,6 @@ export interface TicketCreatedEvent {
         id: string;
         title: string;
         price: number;
+        userId: string;
     };
 }
---
+++ src/events/Subjects.js
@@ -0,0 +1,8 @@
+"use strict";
+exports.__esModule = true;
+exports.Subjects = void 0;
+var Subjects;
+(function (Subjects) {
+    Subjects["TicketCreated"] = "ticket:created";
+    Subjects["OrderUpdated"] = "order:updated";
+})(Subjects = exports.Subjects || (exports.Subjects = {}));
